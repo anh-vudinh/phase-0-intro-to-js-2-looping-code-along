278569695ce9210b17343a111bc211d306716e79
Anh-vu <anhuvdinh@gmail.com>
2021-07-27 16:52:15 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    const messageArray = [];
+    for (let i = 0; i < name.length; i++){
+        messageArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messageArray;
+}
+
+function countDown(){
+    for (let i = 10; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+function countDownWhile(){
+    let countDownNumber = 10;
+    while (countDownNumber >= 0){
+        console.log(countDownNumber);
+        countDownNumber--;
+    }
+}
